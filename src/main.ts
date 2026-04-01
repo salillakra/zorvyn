db3e384dd3baa06a3303ff74f8fb28387db24810
Salil Lakra <salillakra.dev@gmail.com>
2026-04-01 19:03:17 +0530

    init project

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log'], // only log errors, warnings, and general logs
+  });
+
+  const port = Number(process.env.PORT ?? 3000);
+  await app.listen(port);
+}
+
+void bootstrap();
