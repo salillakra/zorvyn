1809bddbf6372cdac261fb5e7db536d81ef1dde1
Salil Lakra <salillakra.dev@gmail.com>
2026-04-02 12:25:12 +0530

    feat: add LoginUserDto for user login validation and enhance UserService with registration and authentication methods

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import * as nestjsBetterAuth from '@thallesp/nestjs-better-auth';
+import { UserService } from './user.service';
+import { RegisterUserDto } from './dto/RegisterUserDto';
 
-@Controller('user')
-export class UserController {}
+@Controller(['users'])
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+  @Get('me')
+  getProfile(
+    @nestjsBetterAuth.Session() session: nestjsBetterAuth.UserSession,
+  ) {
+    return { user: session.user };
+  }
+
+  @Post('register')
+  @nestjsBetterAuth.AllowAnonymous()
+  registerUser(@Body() registerUserDto: RegisterUserDto) {
+    return this.userService.registerUserService(registerUserDto);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,77 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { authApi } from '../../lib/auth';
 
+interface RegisterUserInput {
+  name: string;
+  email: string;
+  password: string;
+  image?: string;
+  callbackURL?: string;
+}
+interface SignInUserInput {
+  email: string;
+  password: string;
+}
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  //Register user service
+  async registerUserService({
+    name,
+    email,
+    password,
+    image,
+    callbackURL,
+  }: RegisterUserInput) {
+    const data = await authApi.signUpEmail({
+      body: {
+        name: name, // required
+        email: email, // required
+        password: password, // required
+        image: image,
+        callbackURL: callbackURL,
+      },
+    });
+    return {
+      success: true,
+      message: 'User registered successfully',
+      data,
+      meta: {
+        timestamp: new Date().toISOString(),
+      },
+    };
+  }
+
+  //Sign in user service
+  async signInUserService({ email, password }: SignInUserInput) {
+    const data = await authApi.signInEmail({
+      body: {
+        email: email, // required
+        password: password, // required
+      },
+    });
+    return {
+      success: true,
+      message: 'User signed in successfully',
+      data,
+      meta: {
+        timestamp: new Date().toISOString(),
+      },
+    };
+  }
+
+  //Sign out user service
+  async signOutUserService() {
+    const data = await authApi.signOut();
+    return {
+      success: true,
+      message: 'User signed out successfully',
+      data,
+      meta: {
+        timestamp: new Date().toISOString(),
+      },
+    };
+  }
+}
