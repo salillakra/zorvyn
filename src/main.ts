b5acbec379664ed41878ed8c0b670e67c06cb469
Salil Lakra <salillakra.dev@gmail.com>
2026-04-03 21:44:10 +0530

    feat: add Swagger documentation setup for API

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'log'], // only log errors, warnings, and general logs
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Zorvyn Screening Assignment API')
+    .setDescription('API documentation for the Zorvyn Screening Assignment')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        description: 'Paste access token as: Bearer <token>',
+      },
+      'bearer',
+    )
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   const port = Number(process.env.PORT ?? 3000);
   await app.listen(port);
