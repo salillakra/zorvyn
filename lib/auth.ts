d5e62122cdc114d00beb01fe13d9be8a272888bc
Salil Lakra <salillakra.dev@gmail.com>
2026-04-03 21:42:43 +0530

    feat: refactor authentication setup to use prismaClient and remove unused code

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,27 +1,18 @@
 import 'dotenv/config';
 import { betterAuth } from 'better-auth';
 import { prismaAdapter } from '@better-auth/prisma-adapter';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '../generated/prisma/client';
-
-const databaseUrl = process.env.DATABASE_URL;
-
-if (!databaseUrl) {
-  throw new Error('DATABASE_URL is required');
-}
-
-const prisma = new PrismaClient({
-  adapter: new PrismaPg(databaseUrl),
-});
+import { bearer } from 'better-auth/plugins/bearer';
+import { prismaClient } from '../src/prisma.service';
 
 export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL ?? 'http://localhost:3000',
-  database: prismaAdapter(prisma, {
+  database: prismaAdapter(prismaClient, {
     provider: 'postgresql',
   }),
   emailAndPassword: {
     enabled: true,
   },
+  plugins: [bearer()],
 });
 
 export const authApi = auth.api;
