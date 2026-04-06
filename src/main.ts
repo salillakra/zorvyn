42f4b0d605e0a30f539df372c68ce8321be4e6c5
Salil Lakra <salillakra.dev@gmail.com>
2026-04-06 12:48:10 +0530

    feat: update start scripts in package.json and pnpm-lock.yaml, add dotenv dependency; modify main.ts to listen on all network interfaces

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap(): Promise<void> {
   SwaggerModule.setup('api', app, documentFactory);
 
   const port = Number(process.env.PORT ?? 3000);
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 }
 
 void bootstrap();
